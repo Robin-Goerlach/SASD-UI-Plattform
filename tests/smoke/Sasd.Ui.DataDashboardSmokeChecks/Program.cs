490d3dc42b58809e32efd5afb93effe287cfcb88
Robin Goerlach (SASD-GmbH) <robin.goerlach@sasd.de>
2026-09-10 18:07:20 +0200

    Test: run grid controller resilience scenarios

--- tests/smoke/Sasd.Ui.DataDashboardSmokeChecks/Program.cs
+++ tests/smoke/Sasd.Ui.DataDashboardSmokeChecks/Program.cs
@@ -14,6 +14,7 @@ internal static class Program
             ValidateSavedGridView();
             ValidatePager();
             await ValidateGridControllerAsync();
+            GridControllerResilienceChecks.Run();
             ValidateSparkline();
             ValidateKpiCard();
 
